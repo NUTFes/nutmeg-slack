0aa46f6bd01956a07948b6eca582e155da4b064d
ryuseifujisaki <fujiryu1122@gmail.com>
2022-12-20 23:29:13 +0900

    整形完了

--- clients/mongodb.py
+++ clients/mongodb.py
@@ -1,3 +1,4 @@
+import os
 from pymongo import MongoClient
 
 # client = MongoClient("mongodb://root:password@mongo:27017")
@@ -8,16 +9,23 @@ from pymongo import MongoClient
 
 class MongoDB:
   def __init__(self, host, port, user, password, db_name, collection_name):
-    """
-    コンストラクタ
+    """ コンストラクタ
+    Args:
+      host (str): ホスト名
+      port (int): ポート番号
+      user (str): ユーザー名
+      password (str): パスワード
+      db_name (str): DB名
+      collection_name (str): コレクション名
     """
     self.client = MongoClient(f"mongodb://{user}:{password}@{host}:{port}")
-    self.db = self.client[db_name]
+    self.db = self.client[os.environ.get("DB")]
     self.collection = self.db[collection_name]
 
-  def insert(self, data):
-      self.collection.insert_one(data)
-
-  def close(self):
-    self.client.close()
+  def insert(self, log):
+    """ DBにデータを挿入する
+      Args:
+        log (dict): 挿入するメッセージログ
+    """
+    self.collection.insert_one(log)
 
--- main.py
+++ main.py
@@ -10,11 +10,13 @@ app = App(
 )
 
 @app.event("message")
-def monitoring_nutfes_slack(body, logger):
+def monitoring_nutfes_slack(body: dict):
+  """ Slackのメッセージを監視する
+  Args:
+    body (dict): Slackのメッセージ
+  """
   mongo = MongoDB("mongo", 27017, "root", "password", "nutfes_slack_log", "log")
   mongo.insert(body)
-  logger.info(body)
-  print(body)
 
 if __name__ == "__main__":
   app.start(port=int(os.environ.get("PORT", 3000)))
