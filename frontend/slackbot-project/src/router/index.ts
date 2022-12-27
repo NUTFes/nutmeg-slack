3e929a755f215ecf6fecac8a2beefd0d630eabcc
ryuseifujisaki <fujiryu1122@gmail.com>
2022-12-27 22:59:53 +0900

    API+page作成完了

--- frontend/slackbot-project/src/router/index.ts
+++ frontend/slackbot-project/src/router/index.ts
@@ -20,7 +20,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/channels",
     name: "channels",
     component: () => import("../views/ChannelView.vue"),
-  }
+  },
 ];
 
 const router = createRouter({
