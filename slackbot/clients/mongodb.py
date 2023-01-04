eb7260391a3aec5b903632fcbadeec14ac8438aa
ryuseifujisaki <fujiryu1122@gmail.com>
2023-01-05 00:57:02 +0900

    DBにchannel名情報を取得できるようにした

--- slackbot/clients/mongodb.py
+++ slackbot/clients/mongodb.py
@@ -1,11 +1,4 @@
-import os
 from pymongo import MongoClient
-
-# client = MongoClient("mongodb://root:password@mongo:27017")
-# db = client.nutfes_slack_log
-# collection_test = db.test
-# collection_test.insert_one({'name': 'test2'})
-# client.close()
 
 class MongoDB:
   def __init__(self, host, port, user, password, db_name, collection_name):
@@ -22,10 +15,24 @@ class MongoDB:
     self.db = self.client[db_name]
     self.collection = self.db[collection_name]
 
-  def insert(self, log):
+  ## https://pymongo.readthedocs.io/en/stable/api/pymongo/collection.html#pymongo.collection.Collection.update_one
+  def insert(self, data:dict):
     """ DBにデータを挿入する
       Args:
-        log (dict): 挿入するメッセージログ
+        data (dict): 挿入するデータ
     """
-    self.collection.insert_one(log)
+    self.collection.insert_one(data)
 
+  def update(self, mapping:dict, replace:dict):
+    """ DBのデータを更新する
+      Args:
+        data (dict): 更新するdata
+    """
+    self.collection.replace_one(mapping, replace)
+
+  def find(self)->dict:
+    """DBのデータを先頭1件取得する
+    Returns:
+      dict: 取得したデータ
+    """
+    return self.collection.find_one({})
--- slackbot/main.py
+++ slackbot/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from pymongo import MongoClient
@@ -15,8 +16,21 @@ def monitoring_nutfes_slack(body: dict):
   Args:
     body (dict): Slackのメッセージ
   """
-  mongo = MongoDB("mongo", 27017, "root", "password",os.environ.get("DB") , "log")
+  mongo = MongoDB("mongo", 27017, "root", "password", os.environ.get("DB"), "log")
   mongo.insert(body)
+  register_channel_name()
+
+def register_channel_name():
+  """ Slackのチャンネル名を{id:name}として登録する
+  """
+  url = "https://slack.com/api/conversations.list"
+  response = requests.get(url, headers={"Authorization": "Bearer " + os.environ.get("SLACK_BOT_TOKEN")})
+  channels = response.json()["channels"]
+  replace_channel_dict = {channel["id"]:channel["name"] for channel in channels}
+
+  mongo = MongoDB("mongo", 27017, "root", "password", os.environ.get("DB"), "channel")
+  registered_channel_dict = mongo.find()
+  mongo.update(registered_channel_dict ,replace_channel_dict)
 
 if __name__ == "__main__":
   app.start(port=int(os.environ.get("PORT", 3000)))
