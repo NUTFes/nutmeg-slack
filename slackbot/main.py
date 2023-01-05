85524d969eb658bfbc73d3849744fea7870c1e5d
ryuseifujisaki <fujiryu1122@gmail.com>
2023-01-05 16:59:42 +0900

    user情報の登録

--- slackbot/main.py
+++ slackbot/main.py
@@ -19,6 +19,7 @@ def monitoring_nutfes_slack(body: dict):
   mongo = MongoDB("mongo", 27017, "root", "password", os.environ.get("DB"), "log")
   mongo.insert(body)
   register_channel_name()
+  register_user_name()
 
 def register_channel_name():
   """ Slackのチャンネル名を{id:name}として登録する
@@ -30,7 +31,24 @@ def register_channel_name():
 
   mongo = MongoDB("mongo", 27017, "root", "password", os.environ.get("DB"), "channel")
   registered_channel_dict = mongo.find()
-  mongo.update(registered_channel_dict ,replace_channel_dict)
+  if registered_channel_dict == None:
+    mongo.insert(replace_channel_dict)
+  mongo.update(registered_channel_dict, replace_channel_dict)
+
+
+def register_user_name():
+  """ Slackのユーザ名を{id:name}として登録する
+  """
+  url = "https://slack.com/api/users.list"
+  response = requests.get(url, headers={"Authorization": "Bearer " + os.environ.get("SLACK_BOT_TOKEN")})
+  users = response.json()["members"]
+  replace_user_dict = {user["id"]:user["profile"]["display_name"] for user in users}
+
+  mongo = MongoDB("mongo", 27017, "root", "password", os.environ.get("DB"), "user")
+  registered_user_dict = mongo.find()
+  if registered_user_dict == None:
+    mongo.insert(replace_user_dict)
+  mongo.update(registered_user_dict, replace_user_dict)
 
 if __name__ == "__main__":
   app.start(port=int(os.environ.get("PORT", 3000)))
