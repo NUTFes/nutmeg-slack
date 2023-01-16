e6b8bcdede8c3a6db93f11bb58665532532ca3d2
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-01-16 17:43:25 +0900

    環境変数修正

--- slackbot/main.py
+++ slackbot/main.py
@@ -12,7 +12,7 @@ app = App(
 
 db_user = os.environ.get("DB_USER")
 db_password = os.environ.get("DB_PASSWORD")
-db_name = os.environ.get("DB")
+db_name = os.environ.get("DB_NAME")
 db_port = os.environ.get("DB_PORT")
 db_host = os.environ.get("DB_HOST")
 
